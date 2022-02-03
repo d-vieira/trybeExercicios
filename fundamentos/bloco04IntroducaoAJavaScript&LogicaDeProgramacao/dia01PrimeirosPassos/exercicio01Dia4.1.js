647d72bef4cedebe9f827990d004874de8df309c
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-03 11:26:16 -0300

    Adiciona conclusao para exercicio 4 em exercicio01Dia4.1.js

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia01PrimeirosPassos/exercicio01Dia4.1.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia01PrimeirosPassos/exercicio01Dia4.1.js
@@ -3,22 +3,36 @@ const b = 72;
 let msgExUm = "Ambos os números são iguais! =]"
 const c = 11;
 let msgExDois = "Todos os números são iguais! =]"
+let msgExTres = null;
 
-  console.log(a + b);
-  console.log(a - b);
-  console.log(a * b);
-  console.log(a / b);
-  console.log(a % b);
+  // console.log(a + b);
+  // console.log(a - b);
+  // console.log(a * b);
+  // console.log(a / b);
+  // console.log(a % b);
 
-  if (a > b && c) {
-    console.log(a);
+  // if (a > b && c) {
+  //   console.log(a);
+  // }
+  // if else (b > a && c) {
+  //   console.log(b);
+  // }
+  // if else (c > a && b) {
+  //   console.log(c);
+  // }
+  // else {
+  //   console.log(msgExDois);
+  // }
+
+  if (b > 0) {
+    msgExTres = "Positive";
+    console.log(msgExTres);
   }
-  if else (b > a && c) {
-    console.log(b);
-  }
-  if else (c > a && b) {
-    console.log(c);
+  else if (b < 0) {
+    msgExTres = "Negative";
+    console.log(msgExTres);
   }
   else {
-    console.log(msgExDois);
+    msgExTres = "Zero";
+    console.log(msgExTres);
   }
