667e73639ebd3396c9dad6c516699c72cd5fb02b
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-03 18:38:07 -0300

    Adiciona conclusao para exercicio 6 em exercicio01dia4.1.js

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia01PrimeirosPassos/exercicio01Dia4.1.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia01PrimeirosPassos/exercicio01Dia4.1.js
@@ -1,10 +1,10 @@
-const a = 27;
-const b = 72;
-let msgExUm = "Ambos os números são iguais! =]"
-const c = 11;
-let msgExDois = "Todos os números são iguais! =]"
-let msgExTres = null;
-const tri = a + b + c;
+// const a = 27;
+// const b = 72;
+// let msgExUm = "Ambos os números são iguais! =]"
+// const c = 11;
+// let msgExDois = "Todos os números são iguais! =]"
+// let msgExTres = null;
+// const tri = a + b + c;
 
   // console.log(a + b);
   // console.log(a - b);
@@ -38,15 +38,24 @@ const tri = a + b + c;
   //   console.log(msgExTres);
   // }
 
-    if (tri == 180) {
-      msgExTres = true;
-      console.log(msgExTres);
-    }
-    else if (tri > 180 || tri < 180) {      
-      msgExTres = false;
-      console.log(msgExTres);
-    }
-    else {
-      msgExTres = "Error!";
-      console.log(msgExTres);
-    }
+    // if (tri == 180) {
+    //   msgExTres = true;
+    //   console.log(msgExTres);
+    // }
+    // else if (tri > 180 || tri < 180) {      
+    //   msgExTres = false;
+    //   console.log(msgExTres);
+    // }
+    // else {
+    //   msgExTres = "Error!";
+    //   console.log(msgExTres);
+    // }
+
+let pawn = "Pawns move vertically forward one square, with the option to move two squares if they have not yet moved. Pawns are the only piece to capture different to how they move. The pawns capture one square diagonally in a forward direction. Pawns are unable to move backward on captures or moves. Upon reaching the other side of the board a pawn promotes into any other piece, except for a king. Additionally, pawns can make a special move named En Passant.";
+let rook = "Rooks move horizontally or vertically any number of squares. They are unable to jump over pieces. Rooks move when the king castles.";
+let knight = "Knights move in an ‘L’ shape’: two squares in a horizontal or vertical direction, then move one square horizontally or vertically. They are the only piece able to jump over other pieces.";
+let bishop = "Bishops move diagonally any number of squares. They are unable to jump over pieces.";
+let queen = "Queens move diagonally, horizontally, or vertically any number of squares. They are unable to jump over pieces.";
+let king = "Kings move one square in any direction, so long as that square is not attacked by an enemy piece. Additionally, kings are able to make a special move, known as castling.";
+
+  console.log(pawn);
