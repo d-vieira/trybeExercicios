1bfc3c5e836cf0b76f06c3e145544bef3d7959b5
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-03 11:03:01 -0300

    Adiciona conclusao para exercicio 3 em exercicio01Dia4.1.js

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia01PrimeirosPassos/exercicio01Dia4.1.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia01PrimeirosPassos/exercicio01Dia4.1.js
@@ -1,6 +1,8 @@
 const a = 27;
 const b = 72;
 let msgExUm = "Ambos os números são iguais! =]"
+const c = 11;
+let msgExDois = "Todos os números são iguais! =]"
 
   console.log(a + b);
   console.log(a - b);
@@ -8,12 +10,15 @@ let msgExUm = "Ambos os números são iguais! =]"
   console.log(a / b);
   console.log(a % b);
 
-  if (a > b) {
+  if (a > b && c) {
     console.log(a);
   }
-  if else (b > a) {
-      console.log(b);
+  if else (b > a && c) {
+    console.log(b);
+  }
+  if else (c > a && b) {
+    console.log(c);
   }
   else {
-      console.log(msgExDois);
+    console.log(msgExDois);
   }
