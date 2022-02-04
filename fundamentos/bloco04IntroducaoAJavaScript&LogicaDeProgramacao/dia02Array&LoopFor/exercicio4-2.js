2606379252ef974e820960b08a9ed00688e334e5
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-04 20:49:07 -0300

    Adiciona conclusao para exercicio 8

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia02Array&LoopFor/exercicio4-2.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia02Array&LoopFor/exercicio4-2.js
@@ -43,17 +43,35 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
                 // // Concluí de forma MORAL com a ajuda do Breno Barbosa Trindade.
 
                 
-// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
-let numbers = [3, 8, 2, 18, 70, 8, 100, 2, 34, 26];
-let result = 0;
-for (index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] % 2 != 0) {
-    result += 1;
-  }
-}
 
-if (result == 0) {
-  console.log("tem nada aqui")
-} else {
-  console.log(result);
+            // let numbers = [3, 8, 2, 18, 70, 8, 100, 2, 34, 26];
+            // let result = 0;
+            // for (index = 0; index < numbers.length; index += 1) {
+            //   if (numbers[index] % 2 != 0) {
+            //     result += 1;
+            //   }
+            // }
+
+            // if (result == 0) {
+            //   console.log("tem nada aqui")
+            // } else {
+            //   console.log(result);
+            
+            
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 102, 35, 27];
+
+// result = 0
+//   for (index = 0; index < numbers.lenght ; index += 1) { 
+//      if (result < numbers[index]) {
+//        result = numbers[index];
+//       }
+//     }
+//     console.log(result);
+
+
+result = [];
+for (let index = 1; index <= 25; index += 1) {
+  result.push(index);
 }
+console.log(result);
