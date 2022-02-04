2186b93ff02c9dacba773cc7057d35bf942a16c2
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-04 11:57:05 -0300

    Adiciona conclusao para exercicio 4

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia02Array&LoopFor/exercicio4-2.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia02Array&LoopFor/exercicio4-2.js
@@ -15,5 +15,9 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     for (let index = 0; index < numbers.length; index += 1) {
       result += numbers[index];
     }
-    
-    console.log(result / numbers.length);
+    if (result / numbers.length > 20) {
+      console.log("valor maior que 20");
+    }
+    else {
+      console.log("valor menor ou igual a 20");
+    }
