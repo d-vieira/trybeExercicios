fbc8e03ae27df14f5a9e081ae99cbdb7a88813b0
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-04 18:47:07 -0300

    conclui exercicio 5

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia02Array&LoopFor/exercicio4-2.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia02Array&LoopFor/exercicio4-2.js
@@ -11,13 +11,33 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
     
   //   console.log(result);
 
-  let result = 0;
-    for (let index = 0; index < numbers.length; index += 1) {
-      result += numbers[index];
-    }
-    if (result / numbers.length > 20) {
-      console.log("valor maior que 20");
-    }
-    else {
-      console.log("valor menor ou igual a 20");
-    }
+  // let result = 0;
+  //   for (let index = 0; index < numbers.length; index += 1) {
+  //     result += numbers[index];
+  //   }
+  //   if (result / numbers.length > 20) {
+  //     console.log("valor maior que 20");
+  //   }
+  //   else {
+  //     console.log("valor menor ou igual a 20");
+  //   }
+
+ 
+// let result = 0;  
+// for (let indexUm = 0; indexUm < numbers.length; indexUm += 1) {
+//    for (let indexDois = 0; indexDois < numbers.length; indexDois += 1) {
+//      if (numbers[indexDois] > numbers[indexUm]) {
+//        result = numbers[indexDois];
+//      }
+//    }
+// }
+// console.log(numbers[indexUm]);
+                let numbers = [5, 9, 3, 19, 70, 8, 100, 102, 35, 27];
+                let maiorValor = 0;
+                for (index =0 ; index < numbers.length; index +=1){
+                  if(numbers[index] > maiorValor){
+                    maiorValor = numbers[index];
+                  }
+                }
+                console.log(maiorValor);
+                // Concluí de forma MORAL com a ajuda do Breno Barbosa Trindade.
