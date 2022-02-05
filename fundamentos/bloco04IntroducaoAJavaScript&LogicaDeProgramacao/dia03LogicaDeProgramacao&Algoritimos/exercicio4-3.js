2eb72d6ed729a4dcf334cf51ecaa348376b7308d
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-05 14:11:01 -0300

    Adiciona resolucao para exercicio 1

---
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia03LogicaDeProgramacao&Algoritimos/exercicio4-3.js
@@ -0,0 +1,9 @@
+let resultadoUm = 10;
+let resultadoDois;
+
+for (let indexUm = 10; indexUm >= 1; indexUm -= 1) {
+  resultadoUm = indexUm * resultadoUm;
+  
+}
+console.log(resultadoUm);
+// Consultar exercicio 1, consolelog gerando 1 zero a mais
