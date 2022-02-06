8aebb3d3f4eaef8ccc9580cb5224ce7a2f7e880e
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-06 19:33:23 -0300

    Adiciona resolucao para exercicio 4

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia03LogicaDeProgramacao&Algoritimos/exercicio4-3.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia03LogicaDeProgramacao&Algoritimos/exercicio4-3.js
@@ -43,7 +43,6 @@
 // console.log(resultOne);
 
 // EXERCICIO 4
-// 4- Um número primo é aquele divisível apenas por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que retorne o maior número primo entre 0 e 50.
 
 let result = 2;
 let input = 50;
