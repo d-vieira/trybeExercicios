17ecf228944ad25f2091db9ed274b21d5cd4eea6
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-06 15:36:02 -0300

    Adiciona resolucao para exercicio 3 parte 1

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia03LogicaDeProgramacao&Algoritimos/exercicio4-3.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia03LogicaDeProgramacao&Algoritimos/exercicio4-3.js
@@ -10,10 +10,24 @@
 // Consultar exercicio 1, consolelog gerando 1 zero a mais
 
 // EXERCICIO 2
-let word = 'tryber';
-let resultOne = ""
-;
-for (let indexOne = word.length -1; indexOne >= 0; indexOne -= 1) {
-  resultOne += word[indexOne];
+// let word = 'tryber';
+// let resultOne = ""
+// ;
+// for (let indexOne = word.length -1; indexOne >= 0; indexOne -= 1) {
+//   resultOne += word[indexOne];
+// }
+// console.log(resultOne);
+
+// EXERCICIO 3
+// Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
+
+let array = ["java", "javascript", "python", "html", "css",];
+
+let resultOne = array[0];
+
+for (let index1 = 0; index1 < array.length; index1 += 1) {
+  if (array[index1].length > resultOne.length) {
+    resultOne = array[index1];
+  } 
 }
 console.log(resultOne);
