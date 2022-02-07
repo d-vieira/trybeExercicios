d57c963f7cbec9ed36272a1410c458e15c722072
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-07 19:06:20 -0300

    Adiciona conclusao para exercicio 5 em exercicios4-4-pt1.js

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia04Objetos&Funcoes/exercicios4-4-pt1.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia04Objetos&Funcoes/exercicios4-4-pt1.js
@@ -28,13 +28,33 @@
 // }
 
 // 04-
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+// };
+// info["recorrente"] = "Sim";
+
+// for (let key in info) {
+//   console.log(info[key]);
+// }
+
+// 05-
+let infoDois = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim",
+};
+
 let info = {
   personagem: "Margarida",
   origem: "Pato Donald",
   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: "Sim",
 };
-info["recorrente"] = "Sim";
+for (let key in info, infoDois) {
+  console.log(info[key] + " e " + infoDois[key]);
+};
 
-for (let key in info) {
-  console.log(info[key]);
-}
+
