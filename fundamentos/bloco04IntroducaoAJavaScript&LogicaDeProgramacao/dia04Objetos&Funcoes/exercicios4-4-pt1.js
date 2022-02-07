1c3cf1a05557b31cc18d55e314725ab6cba81751
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-07 19:28:34 -0300

    Adiciona conclusao para exercicio 6 em exercicios4-4-pt1.js

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia04Objetos&Funcoes/exercicios4-4-pt1.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia04Objetos&Funcoes/exercicios4-4-pt1.js
@@ -40,21 +40,34 @@
 // }
 
 // 05-
-let infoDois = {
-  personagem: "Tio Patinhas",
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: "O último MacPatinhas",
-  recorrente: "Sim",
-};
+// let infoDois = {
+//   personagem: "Tio Patinhas",
+//   origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+//   nota: "O último MacPatinhas",
+//   recorrente: "Sim",
+// };
 
-let info = {
-  personagem: "Margarida",
-  origem: "Pato Donald",
-  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
-  recorrente: "Sim",
-};
-for (let key in info, infoDois) {
-  console.log(info[key] + " e " + infoDois[key]);
-};
+// let info = {
+//   personagem: "Margarida",
+//   origem: "Pato Donald",
+//   nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+//   recorrente: "Sim",
+// };
+// for (let key in info, infoDois) {
+//   console.log(info[key] + " e " + infoDois[key]);
+// };
 
-
+// 06-
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo);
