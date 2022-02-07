af2abd3a04c858825529dae6afd5803221cf0c3b
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-07 19:46:37 -0300

    Adiciona conclusao para exercicio 8 em exercicios4-4-pt1.js

--- fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia04Objetos&Funcoes/exercicios4-4-pt1.js
+++ fundamentos/bloco04IntroducaoAJavaScript&LogicaDeProgramacao/dia04Objetos&Funcoes/exercicios4-4-pt1.js
@@ -73,6 +73,26 @@
 // console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo);
 
 // 07-
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
+// leitor["livrosFavoritos"].push({
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editora: 'Rocco',
+// })
+// console.log(leitor.livrosFavoritos);
+
+// 08-
 let leitor = {
   nome: 'Julia',
   sobrenome: 'Pessoa',
@@ -83,11 +103,11 @@ let leitor = {
       autor: 'Daniela Kopsch',
       editora: 'Tordesilhas',
     },
+    {
+      titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+      autor: 'JK Rowling',
+      editora: 'Rocco',
+    }
   ],
 };
-leitor["livrosFavoritos"].push({
-  titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-  autor: 'JK Rowling',
-  editora: 'Rocco',
-})
-console.log(leitor.livrosFavoritos);
+console.log(leitor.nome + " tem " + leitor.livrosFavoritos.length + " livros favoritos.");
