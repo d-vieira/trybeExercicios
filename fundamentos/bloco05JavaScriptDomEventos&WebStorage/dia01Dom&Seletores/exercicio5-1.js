635f0af9a4bcc7d566291aece439eec16b1720e7
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-09 20:51:00 -0300

    Adiciona resolucao para exercicio#3

--- fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia01Dom&Seletores/exercicio5-1.js
+++ fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia01Dom&Seletores/exercicio5-1.js
@@ -3,5 +3,9 @@ let changeText = document.getElementsByTagName('p');
 changeText[0].innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac faucibus sapien, elementum viverra erat.";
 
 //2
-let changeMainColor = document.getElementsByClassName('main-content')
-changeMainColor[0].style.backgroundColor = "rgb(76,164,109)";
+let changeMainColor = document.getElementsByClassName('main-content');
+changeMainColor[0].style.backgroundColor = 'rgb(76,164,109)';
+
+//3
+let changeCenterColor = document.getElementsByClassName('center-content');
+changeCenterColor[0].style.backgroundColor = 'rgb(255,255,255)';
