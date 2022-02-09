9f649d0327e2d1c7f088a9bf3bab25414ef4e285
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-09 19:15:08 -0300

    Adiciona arquivo exercicio5-1.js; Adiciona resolucao para exercicio#1

---
+++ fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia01Dom&Seletores/exercicio5-1.js
@@ -0,0 +1,3 @@
+//1
+let changeText = document.getElementsByTagName('p');
+changeText[0].innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac faucibus sapien, elementum viverra erat.";
