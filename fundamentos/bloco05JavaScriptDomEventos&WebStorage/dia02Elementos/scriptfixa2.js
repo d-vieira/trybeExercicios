2da7b1f03f63c365801bf033717c7e3cdde7b340
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-10 18:13:36 -0300

    Adiciona resolucao de exercicio de fixacao

---
+++ fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia02Elementos/scriptfixa2.js
@@ -0,0 +1,8 @@
+document.querySelector('#elementoOndeVoceEsta');
+document.querySelector('#elementoOndeVoceEsta').parentElement.style.color = "blue";
+document.querySelector('#primeiroFilhoDoFilho').innerText = "APENAS UM TEXTO";
+document.querySelector('#pai').firstElementChild;
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
+document.querySelector('#elementoOndeVoceEsta').nextSibling;
+document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
+document.querySelector('#pai').firstElementChild.nextElementSibling.nextElementSibling;
