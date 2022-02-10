1d64e2ce85967f6b2edd19ff3b44c2f184f6b47e
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-09 22:29:58 -0300

    Adiciona resolucao para exercicio#6; Refatora exercicios5-1.js

--- fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia01Dom&Seletores/exercicio5-1.js
+++ fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia01Dom&Seletores/exercicio5-1.js
@@ -1,21 +1,45 @@
 //1
-let changeText = document.getElementsByTagName('p');
-changeText[0].innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac faucibus sapien, elementum viverra erat.";
+function changeText() {
+  let paragraphChange = document.getElementsByTagName('p');
+  paragraphChange[0].innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ac faucibus sapien, elementum viverra erat.";
+}
+changeText();
 
 //2
-let changeMainColor = document.getElementsByClassName('main-content');
-changeMainColor[0].style.backgroundColor = 'rgb(76,164,109)';
+function changeMainColor() {
+  let colorChange = document.getElementsByClassName('main-content');
+  colorChange[0].style.backgroundColor = 'rgb(76,164,109)';
+}
+changeMainColor();
 
 //3
-let changeCenterColor = document.getElementsByClassName('center-content');
-changeCenterColor[0].style.backgroundColor = 'rgb(255,255,255)';
+function changeCenterColor() {
+  let othername = document.getElementsByClassName('center-content');
+  othername[0].style.backgroundColor = 'rgb(255,255,255)';
+}
+changeCenterColor();
 
 //4
-let changeTitle = document.getElementsByTagName('h1');
-changeTitle[0].innerText = 'Exercício 5.1 - JavaScript '
+function changeTitle() {
+  let newTitle = document.getElementsByTagName('h1');
+  newTitle[0].innerText = 'Exercício 5.1 - JavaScript '
+}
+changeTitle();
 
 //5
-let changeToUpperCase = document.getElementsByTagName('p');
-for (let key in changeToUpperCase) {
-  changeToUpperCase[key].style.textTransform = 'uppercase';
+function changeToUpperCase() {
+  let changeCase = document.getElementsByTagName('p');
+  for (let key in changeCase) {
+    changeCase[key].style.textTransform = 'uppercase';
+  }
 }
+changeToUpperCase();
+
+//6
+function logToTheConsole () {
+  let paragraphs = document.getElementsByTagName('p');
+  for (let key in paragraphs) {
+    console.log(paragraphs[key]);
+  }
+}
+logToTheConsole();
