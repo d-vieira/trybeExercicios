245789b6492bb546da95fa258da3320eb13c38b4
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-11 14:29:26 -0300

    Adiciona resolucao para exercicio de fixacao 2

--- fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia01Dom&Seletores/fix2.js
+++ fundamentos/bloco05JavaScriptDomEventos&WebStorage/dia01Dom&Seletores/fix2.js
@@ -1,14 +1,39 @@
+function changeMainTitle() {
+  let titleChange = document.querySelector('#header-container');
+  titleChange.className = 'colorSmoothGreen';
+}
+changeMainTitle();
+
 function changeSubTitleLeftSide() {
   let subtls = document.querySelectorAll('.emergency-tasks h3')
-  
-  
+  for (let key in subtls) {
+  subtls[key].className = 'colorBrightPink';
+  }
 }
 changeSubTitleLeftSide();
 
+function changeSubTitleRightSide() {
+  let subtls2 = document.querySelectorAll('.no-emergency-tasks h3')
+  for (let key in subtls2) {
+  subtls2[key].className = 'colorBlack';
+  }
+}
+changeSubTitleRightSide();
 
-#CF8E80 (smooth pink hex)
-#ED31B1 (bright pink)
-#13020D (black)
-#FFFFFF (white)
-#1B4332 (dark green)
-#52B788 (smooth green)
+function changeFooter() {
+  let footer = document.querySelector('#footer-container');
+  footer.className = 'colorDarkGreen';
+}
+changeFooter();
+
+function changeSubTitleLeftSideBg() {
+  let subtlsBg = document.querySelector('.emergency-tasks')
+  subtlsBg.className = 'emergency-tasks colorSmoothPink';
+}
+changeSubTitleLeftSideBg();
+  
+function changeSubTitleRightSideBg() {
+  let subtlsBg2 = document.querySelector('.no-emergency-tasks')
+  subtlsBg2.className = 'no-emergency-tasks colorSmoothYellow';
+}
+changeSubTitleRightSideBg();
