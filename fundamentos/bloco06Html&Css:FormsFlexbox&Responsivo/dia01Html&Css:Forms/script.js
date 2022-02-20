d43fda881bf1d97d299fd129f5f8cb5642d57188
Daniel Vieira <dann-vieira@hotmail.com>
2022-02-20 09:42:36 -0300

    Adiciona resolucao para requisitos bonus

--- fundamentos/bloco06Html&Css:FormsFlexbox&Responsivo/dia01Html&Css:Forms/script.js
+++ fundamentos/bloco06Html&Css:FormsFlexbox&Responsivo/dia01Html&Css:Forms/script.js
@@ -1,7 +1,17 @@
 let mySubmitButton = document.querySelector('#submit-button');
-mySubmitButton.addEventListener('click', stopSubmitButton);
+// mySubmitButton.addEventListener('click', stopSubmitButton);
+mySubmitButton.addEventListener('click', validateFormInputs)
+
+function validateFormInputs(event) {
+  let myFormNameInput = document.forms['trybe-trip']['full-name'].value;
+  let myFormEmailInput = document.forms['trybe-trip']['e-mail-address'].value;
+  if (myFormNameInput.length < 10 || myFormEmailInput.length < 10) {
+    window.alert('Dados Inválidos');
+  } else {
+    window.alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.');
+  }
+}
 
 // function stopSubmitButton(event) {
 //   event.preventDefault();
 // }
-
