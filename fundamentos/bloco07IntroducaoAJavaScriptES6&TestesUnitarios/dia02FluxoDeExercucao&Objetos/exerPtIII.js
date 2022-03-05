329b8cef6170bf6ea6f3f21fc8b9d4a05ad1484d
Daniel Vieira <dann-vieira@hotmail.com>
2022-03-05 18:36:20 -0300

    Adiciona resolucao para requisitos 1, 2 e 3 em exerPtIII.js

--- fundamentos/bloco07IntroducaoAJavaScriptES6&TestesUnitarios/dia02FluxoDeExercucao&Objetos/exerPtIII.js
+++ fundamentos/bloco07IntroducaoAJavaScriptES6&TestesUnitarios/dia02FluxoDeExercucao&Objetos/exerPtIII.js
@@ -17,3 +17,29 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
+
+// function addTurnOnLesson2 (object, key, value) {
+//   const newKey = key;
+//   const newValue = value;
+//   object[newKey] = newValue;
+//   // return object;
+// }
+// console.log(addTurnOnLesson2(lesson2, 'Turno', 'Noite'));
+// addTurnOnLesson2(lesson2, 'Turno', 'Noite');
+// console.log(lesson2);
+// const createTurn = (object, key, value) => object[key] = value; // Função criada pelo Breno.
+
+// function listOfKeys (object) {
+//   const list = Object.keys(object);
+//   return list;
+// }
+// console.log(listOfKeys(lesson3));
+// const listKey = (object) => Object.keys(object); // Função criada pelo Breno.
+
+// function calcObjectLength (object) {
+//   const count = Object.keys(object);
+//   return count.length;
+// }
+// console.log(calcObjectLength(lesson1));
+// const lenghtObject = (object) => Object.keys(object).length;
+// console.log(lenghtObject(lesson1));
