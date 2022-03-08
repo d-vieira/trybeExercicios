f1635d3dd9cbfa7775d4103252c9a9697671b8da
Daniel Vieira <dann-vieira@hotmail.com>
2022-03-08 12:43:01 -0300

    Adiciona resolucao para requisito 2

--- fundamentos/bloco07IntroducaoAJavaScriptES6&TestesUnitarios/dia03PrimeirosPassosEmJest/script.test.js
+++ fundamentos/bloco07IntroducaoAJavaScriptES6&TestesUnitarios/dia03PrimeirosPassosEmJest/script.test.js
@@ -1,4 +1,4 @@
-const {sum, } = require('./script');
+const {sum, myRemove, } = require('./script');
 
 describe('Função para somar dois parâmetros', () => {
   it ('Retorna 9 ao somar 4 e 5', () => {
@@ -11,6 +11,21 @@ describe('Função para somar dois parâmetros', () => {
 
   it ('Lança erro caso algum dos parâmetros não seja do tipo número', () => {
     expect(() => {sum(4,'5')}).toThrow('parameters must be numbers');
+  })
+
+});
+
+describe ('Função para remover item de um array', () => {
+  it ('Verifica se a função retorna o array conforme esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  })
+  
+  it ('Removeu o item "3" do array', () => {
+    expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+  })
+
+  it ('Verifica se a função retorna o array conforme esperado', () => {
+    expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
   })
 
 });
