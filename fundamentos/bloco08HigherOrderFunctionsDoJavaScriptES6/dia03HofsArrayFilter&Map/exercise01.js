88f35ad3b2cdedec32e2e054b7fa8eb2f018b324
Daniel Vieira <dann-vieira@hotmail.com>
2022-03-13 15:28:11 -0300

    Adiciona resolucao para exercise01.js

--- fundamentos/bloco08HigherOrderFunctionsDoJavaScriptES6/dia03HofsArrayFilter&Map/exercise01.js
+++ fundamentos/bloco08HigherOrderFunctionsDoJavaScriptES6/dia03HofsArrayFilter&Map/exercise01.js
@@ -77,5 +77,6 @@ const expectedResult = [
 ];
 
 function formatedBookNames() {
-  // escreva seu código aqui
+  return books.map((book) => `${book.name} - ${book.genre} - ${book.author.name}`);
 }
+console.log(formatedBookNames());
