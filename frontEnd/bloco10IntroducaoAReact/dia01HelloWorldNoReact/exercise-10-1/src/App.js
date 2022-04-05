1efee2abcc3826016075696d62580f75d2e3f04c
Daniel Vieira <dann-vieira@hotmail.com>
2022-04-05 09:56:58 -0300

    Adiciona diretorios e estrutura para exercicios 10.1

---
+++ frontEnd/bloco10IntroducaoAReact/dia01HelloWorldNoReact/exercise-10-1/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './App.css';
+import Fixacao from './Component';
+
+class App extends Component {
+  render () {
+    return <Fixacao/>
+  };
+}
+
+export default App;
---
+++ frontEnd/bloco10IntroducaoAReact/dia01HelloWorldNoReact/exercise-10-1/src/Component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// Exercício de fixação
+class Fixacao extends Component { 
+  render () {
+    return <div>
+      <h1>Daniel Vieira</h1>
+      <p>Começou a aprender "react" no dia 04 de abril de 2022</p>
+      </div>
+  }
+}
+
+export default Fixacao;
