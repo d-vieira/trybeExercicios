0d59ee1fb3f620514348a31ed3552bb2b991938e
Daniel Vieira <dann-vieira@hotmail.com>
2022-04-06 13:14:07 -0300

    Adiciona resolucao para requisito03

--- frontEnd/bloco10IntroducaoAReact/dia01HelloWorldNoReact/exercise-10-1-requisito-3/src/App.js
+++ frontEnd/bloco10IntroducaoAReact/dia01HelloWorldNoReact/exercise-10-1-requisito-3/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Content from './Components/Content';
+import Footer from './Components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header/>
+        <Content/>
+        <Footer/>
       </div>
     );
   }
